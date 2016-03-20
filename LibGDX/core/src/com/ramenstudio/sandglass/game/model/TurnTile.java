dc828e3e9aa5cf6d2b19e33b20746b2d3f1a9ec6
Jiacong Xu <jiacong.xu@foxmail.com>
2016-03-20 17:35:08 -0400

    Created object marker for turning points.

---
+++ LibGDX/core/src/com/ramenstudio/sandglass/game/model/TurnTile.java
@@ -0,0 +1,10 @@
+package com.ramenstudio.sandglass.game.model;
+
+/**
+ * A tile that marks a place that player should be able to turn.
+ * 
+ * @author Jiacong Xu
+ */
+public class TurnTile extends GameObject {
+
+}
