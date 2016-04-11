789d85ebfe3e9467ba0d2da348e5631d2f0718f9
Saerom Choi <saerom@dhcp-gs-2815.redrover.cornell.edu>
2016-04-10 21:28:29 -0400

    sp2tab

--- LibGDX/core/src/com/ramenstudio/sandglass/game/controller/OverMonController.java
+++ LibGDX/core/src/com/ramenstudio/sandglass/game/controller/OverMonController.java
@@ -16,26 +16,26 @@ import com.ramenstudio.sandglass.game.model.Monster.Move;
  * 
  */
 public class OverMonController extends MonsterController {
- 
-    // Instance Attributes
-    /** is player in the same world*/
-    public boolean setGoal;
-    /**
-     * Creates an AIController for the monster with the given id.
-     *
-     * @param id The unique monster identifier
-     * @param argMonster is the monster this controller represents
-     * @param gmodel is the board state used for tracking
-     * @param player is the target of this monster
-     */
-    public OverMonController(Monster monster) {
-        super(monster);
-        action = Move.LEFT;
-    }
-    
 
-    
-    public boolean isGrounded() {
+	// Instance Attributes
+	/** is player in the same world*/
+	public boolean setGoal;
+	/**
+	 * Creates an AIController for the monster with the given id.
+	 *
+	 * @param id The unique monster identifier
+	 * @param argMonster is the monster this controller represents
+	 * @param gmodel is the board state used for tracking
+	 * @param player is the target of this monster
+	 */
+	public OverMonController(Monster monster) {
+		super(monster);
+		action = Move.LEFT;
+	}
+
+
+
+	public boolean isGrounded() {
 
 
 		Vector2 g = delegate.getGravity().nor();
@@ -64,129 +64,130 @@ public class OverMonController extends MonsterController {
 		oneFrameRayHandler = handler;
 		return handler.isGrounded;
 	}
-    
-    public boolean isWall(){
-            Vector2 left = delegate.getGravity().nor().rotate90(1);
-            Vector2 right = delegate.getGravity().nor().rotate90(-1);
-            Vector2 leftPos = monster.getBody().getPosition().add(-monster.getSize().scl(0.5f).x,0);
-            Vector2 leftEnd = leftPos.cpy().sub(left.cpy().scl(1.4f));
-            Vector2 rightPos = monster.getBody().getPosition().add(monster.getSize().scl(0.5f).x,0);
-            Vector2 rightEnd = rightPos.cpy().add(right.cpy().scl(1.4f));
-    
-            RayCastHandler handler = new RayCastHandler();
-            RayCastHandler handler2 =new RayCastHandler();
-            delegate.rayCast(handler, leftPos, leftEnd);
-            delegate.rayCast(handler2, rightPos, rightEnd);
-            oneFrameRayHandler = handler;
-            return handler.isGrounded || handler2.isGrounded;
-    }
-    
-    public void rotateMonster(){
-        boolean gdd = isGrounded();
-        boolean wdd = isWall();
-        switch (monster.angle){
-        case EAST:
-            if (action==Move.UP && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
-                action = Move.LEFT;
-            }
-            else if (action==Move.UP && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
-                action = Move.RIGHT;
-            }
-            else if (action==Move.DOWN && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
-                action = Move.LEFT;
-            }
-            else if (action==Move.DOWN && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
-                action = Move.RIGHT;
-            }
-            break;
-        case NORTH:
-            if (action==Move.LEFT && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
-                action = Move.DOWN;
-            }
-            else if (action==Move.LEFT && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
-                action = Move.UP;
-            }
-            else if (action==Move.RIGHT && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
-                action = Move.DOWN;
-            }
-            else if (action==Move.RIGHT && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
-                action = Move.UP;
-            }
-            break;
-        case SOUTH:
-            if (action==Move.LEFT && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
-                action = Move.UP;
-            }
-            else if (action==Move.LEFT && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
-                action = Move.DOWN;
-            }
-            else if (action==Move.RIGHT && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
-                action = Move.UP;
-            }
-            else if (action==Move.RIGHT && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
-                action = Move.DOWN;
-            }
-            break;
-        case WEST:
-            if (action==Move.UP && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
-                action = Move.RIGHT;
-            }
-            else if (action==Move.UP && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
-                action = Move.LEFT;
-            }
-            else if (action==Move.DOWN && !gdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
-                action = Move.RIGHT;
-            }
-            else if (action==Move.DOWN && wdd){
-                monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
-                action = Move.LEFT;
-            }
-            break;
-        default:
-            break;
-        
-        }
-    }
-    /**
-     * Returns the action selected by this MonsterController
-     * 
-     * 
-     * @return the action selected by this MonsterController
-     */
-    public void getAction(float dt) {
-        // Increment the number of ticks.
-        Move move = Move.NONE;
-        ticks++;
-        // Do not need to rework ourselves every frame. Just every 10 ticks.
-            if (setGoal){
-                    String gdd = isGrounded() ? "is Grounded" : "not Grounded";
-                    String wdd = isWall()? "is Walled" : "not Walled";
-                    System.out.println(gdd);
-                    System.out.println(wdd);
-                    move = Move.LEFT;
-            }
-        action = move;
-    }
-    
-    @Override
-    public void update(float dt){
-        rotateMonster();
-        super.update(dt);
-    }
-    
+
+	public boolean isWall(){
+		Vector2 left = delegate.getGravity().nor().rotate90(1);
+		Vector2 right = delegate.getGravity().nor().rotate90(-1);
+		Vector2 leftPos = monster.getBody().getPosition().add(-monster.getSize().scl(0.5f).x,0);
+		Vector2 leftEnd = leftPos.cpy().sub(left.cpy().scl(1.4f));
+		Vector2 rightPos = monster.getBody().getPosition().add(monster.getSize().scl(0.5f).x,0);
+		Vector2 rightEnd = rightPos.cpy().add(right.cpy().scl(1.4f));
+
+		RayCastHandler handler = new RayCastHandler();
+		RayCastHandler handler2 =new RayCastHandler();
+		delegate.rayCast(handler, leftPos, leftEnd);
+		delegate.rayCast(handler2, rightPos, rightEnd);
+		oneFrameRayHandler = handler;
+		return handler.isGrounded || handler2.isGrounded;
+	}
+
+	public void rotateMonster(){
+		boolean gdd = isGrounded();
+		boolean wdd = isWall();
+		switch (monster.angle){
+			case EAST:
+				if (action==Move.UP && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
+					action = Move.LEFT;
+				}
+				else if (action==Move.UP && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
+					action = Move.RIGHT;
+				}
+				else if (action==Move.DOWN && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
+					action = Move.LEFT;
+				}
+				else if (action==Move.DOWN && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
+					action = Move.RIGHT;
+				}
+				break;
+			case NORTH:
+				if (action==Move.LEFT && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
+					monster.getBody().getPosition().set(monster.getBody().getPosition().sub(0,monster.getSize().y*0.5f));
+					action = Move.DOWN;
+				}
+				else if (action==Move.LEFT && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
+					action = Move.UP;
+				}
+				else if (action==Move.RIGHT && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
+					action = Move.DOWN;
+				}
+				else if (action==Move.RIGHT && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
+					action = Move.UP;
+				}
+				break;
+			case SOUTH:
+				if (action==Move.LEFT && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
+					action = Move.UP;
+				}
+				else if (action==Move.LEFT && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
+					action = Move.DOWN;
+				}
+				else if (action==Move.RIGHT && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.EAST));
+					action = Move.UP;
+				}
+				else if (action==Move.RIGHT && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.WEST));
+					action = Move.DOWN;
+				}
+				break;
+			case WEST:
+				if (action==Move.UP && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
+					action = Move.RIGHT;
+				}
+				else if (action==Move.UP && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
+					action = Move.LEFT;
+				}
+				else if (action==Move.DOWN && !gdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.SOUTH));
+					action = Move.RIGHT;
+				}
+				else if (action==Move.DOWN && wdd){
+					monster.setRotation(AngleEnum.convertToAngle(AngleEnum.NORTH));
+					action = Move.LEFT;
+				}
+				break;
+			default:
+				break;
+
+		}
+	}
+	/**
+	 * Returns the action selected by this MonsterController
+	 * 
+	 * 
+	 * @return the action selected by this MonsterController
+	 */
+	public void getAction(float dt) {
+		// Increment the number of ticks.
+		Move move = Move.NONE;
+		ticks++;
+		// Do not need to rework ourselves every frame. Just every 10 ticks.
+		if (setGoal){
+			String gdd = isGrounded() ? "is Grounded" : "not Grounded";
+			String wdd = isWall()? "is Walled" : "not Walled";
+			System.out.println(gdd);
+			System.out.println(wdd);
+			move = Move.LEFT;
+		}
+		action = move;
+	}
+
+	@Override
+	public void update(float dt){
+		rotateMonster();
+		super.update(dt);
+	}
+
 }
