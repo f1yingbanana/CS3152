44318879809f5c295168d4ab66c1c4de294c8086
filthydoughboy <jp882@cornell.edu>
2016-04-24 20:07:41 -0400

    fixed monster orientations

--- LibGDX/core/src/com/ramenstudio/sandglass/game/model/Monster.java
+++ LibGDX/core/src/com/ramenstudio/sandglass/game/model/Monster.java
@@ -155,9 +155,9 @@ public class Monster extends GameObject implements Drawable{
 		
 		parametrizeVertices();
 		
-		System.out.println(orientationsOnPath);
+		System.out.println("These are orientations: " + orientationsOnPath);
 		
-		System.out.println(isLoop);
+		System.out.println("Whether this is a loop or not: " + isLoop);
 	}
 	
 	/** 
@@ -179,8 +179,8 @@ public class Monster extends GameObject implements Drawable{
 			previousMove = moveBetweenTwoVertices(currentVertex, nextVertex);
 		}
 		for (int i = 1; i < vertices.size - 1; i++) {
-			Vector2 currentVertex = vertices.get(i%(vertices.size - 1));
-			Vector2 nextVertex = vertices.get((i + 1)%(vertices.size - 1));
+			Vector2 currentVertex = vertices.get(i%(vertices.size));
+			Vector2 nextVertex = vertices.get((i + 1)%(vertices.size));
 			Move currentMove = moveBetweenTwoVertices(currentVertex, nextVertex);
 			currentOrientation = orientationAfterMove(currentOrientation, 
 					previousMove, currentMove);
