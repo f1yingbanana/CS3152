d0b6096783b23a5a44e658b032f3733174f6ab70
Saerom Choi <saerom@dhcp-mcfad-2270.redrover.cornell.edu>
2016-05-17 14:50:19 -0400

    levels

---
+++ LibGDX/core/assets/Levels/ground tile 1-01.tsx
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground tile 1-01" tilewidth="128" tileheight="128" tilecount="88" columns="11">
+ <image source="ground tile 1-01.png" width="1408" height="1024"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0">
+  <properties>
+   <property name="Collision" value="top_left"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Collision" value="top"/>
+  </properties>
+ </tile>
+ <tile id="2" terrain=",,0,0">
+  <properties>
+   <property name="Collision" value="top"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Collision" value="top_right"/>
+  </properties>
+ </tile>
+ <tile id="11" terrain=",0,,0">
+  <properties>
+   <property name="Collision" value="left"/>
+  </properties>
+ </tile>
+ <tile id="12" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14">
+  <properties>
+   <property name="Collision" value="right"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Collision" value="top_left"/>
+  </properties>
+ </tile>
+ <tile id="17" terrain=",,0,0">
+  <properties>
+   <property name="Collision" value="top"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Collision" value="top"/>
+  </properties>
+ </tile>
+ <tile id="19" terrain=",,0,">
+  <properties>
+   <property name="Collision" value="top_right"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Collision" value="left"/>
+  </properties>
+ </tile>
+ <tile id="23" terrain="0,0,0,0"/>
+ <tile id="24" terrain="0,0,0,0"/>
+ <tile id="25" terrain="0,,0,">
+  <properties>
+   <property name="Collision" value="right"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Collision" value="left"/>
+  </properties>
+ </tile>
+ <tile id="30" terrain="0,,0,">
+  <properties>
+   <property name="Collision" value="right"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Collision" value="bottom_left"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Collision" value="bottom"/>
+  </properties>
+ </tile>
+ <tile id="35" terrain="0,0,,">
+  <properties>
+   <property name="Collision" value="bottom"/>
+  </properties>
+ </tile>
+ <tile id="36" terrain="0,,,">
+  <properties>
+   <property name="Collision" value="bottom_right"/>
+  </properties>
+ </tile>
+ <tile id="38" terrain=",0,,0">
+  <properties>
+   <property name="Collision" value="left"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="Collision" value="right"/>
+  </properties>
+ </tile>
+ <tile id="49" terrain=",0,,">
+  <properties>
+   <property name="Collision" value="bottom_left"/>
+  </properties>
+ </tile>
+ <tile id="50" terrain="0,0,,">
+  <properties>
+   <property name="Collision" value="bottom"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Collision" value="bottom"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="Collision" value="bottom_right"/>
+  </properties>
+ </tile>
+ <tile id="55" terrain=",0,0,0">
+  <properties>
+   <property name="Collision" value="inside_top_left"/>
+  </properties>
+ </tile>
+ <tile id="56" terrain="0,,0,0">
+  <properties>
+   <property name="Collision" value="inside_top_right"/>
+  </properties>
+ </tile>
+ <tile id="66" terrain="0,0,,0">
+  <properties>
+   <property name="Collision" value="inside_bottom_left"/>
+  </properties>
+ </tile>
+ <tile id="67" terrain="0,0,0,">
+  <properties>
+   <property name="Collision" value="inside_bottom_right"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ LibGDX/core/assets/Levels/thickGround.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="thickGround" tilewidth="128" tileheight="128" tilecount="88" columns="11">
+ <image source="thickGround.png" width="1408" height="1024"/>
+</tileset>
