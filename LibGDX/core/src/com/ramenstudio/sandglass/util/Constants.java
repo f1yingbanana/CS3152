03bab6f2872b2c7b08a2606997a93538ef00e9e9
Jiacong Xu <jiacong.xu@foxmail.com>
2016-05-19 20:13:24 -0400

    Deleted some resources

---
+++ LibGDX/core/src/com/ramenstudio/sandglass/util/Constants.java
@@ -0,0 +1,9 @@
+package com.ramenstudio.sandglass.util;
+
+/**
+ * One constant for the game.
+ * @author Jiacong Xu
+ */
+public class Constants {
+  public final String PREF_FILE_PATH = "sandglass_scores";
+}
